8d7e8f00fc34f0eab78c57992335093ed597d227
P.Ramsai <ponugotiramsai@gmail.com>
2024-10-06 16:04:06 +0530

    dma and problem statement

--- src/exception.py
+++ src/exception.py
@@ -19,3 +19,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__=="__main__":
+    try:
+        a=1/10
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
